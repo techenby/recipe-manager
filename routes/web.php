435b9543454add0b827be9acbc70c9eb4ad4b61f
Andy M. Newhouse <hi@andymnewhouse.me>
2020-12-14 21:27:39 -0600

    Reorder routes

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/login');
-Route::get('/recipes', App\Http\Livewire\Recipes::class)->name('recipes');
-Route::get('/recipes/{recipe}', App\Http\Livewire\Recipes\Show::class)->name('recipes.show');
 
 Route::middleware(['auth:sanctum'])->group(function() {
     Route::redirect('/dashboard', '/recipes');
     Route::get('/recipes/create', App\Http\Livewire\Recipes\Create::class)->name('recipes.create');
     Route::get('/recipes/{recipe}/edit', App\Http\Livewire\Recipes\Edit::class)->name('recipes.edit');
 });
+
+Route::get('/recipes', App\Http\Livewire\Recipes::class)->name('recipes');
+Route::get('/recipes/{recipe}', App\Http\Livewire\Recipes\Show::class)->name('recipes.show');
