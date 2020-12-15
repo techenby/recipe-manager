322c676904eea55cc7a56467d4ef0a673fa0a41b
Andy M. Newhouse <hi@andymnewhouse.me>
2020-12-14 21:25:47 -0600

    Remove verifications

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Route::redirect('/', '/login');
 Route::get('/recipes', App\Http\Livewire\Recipes::class)->name('recipes');
 Route::get('/recipes/{recipe}', App\Http\Livewire\Recipes\Show::class)->name('recipes.show');
 
-Route::middleware(['auth:sanctum', 'verified'])->group(function() {
+Route::middleware(['auth:sanctum'])->group(function() {
     Route::redirect('/dashboard', '/recipes');
     Route::get('/recipes/create', App\Http\Livewire\Recipes\Create::class)->name('recipes.create');
     Route::get('/recipes/{recipe}/edit', App\Http\Livewire\Recipes\Edit::class)->name('recipes.edit');
